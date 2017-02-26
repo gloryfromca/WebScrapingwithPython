0b336fddead214f78b01d8069835feb516115c5b
zhanghui <huizhang1995@gmail.com>
2017-02-27 00:20:54 +0800

    a little change from previous one

--- web_scraping.py
+++ web_scraping.py
@@ -26,20 +26,24 @@ def downloadpath(url,baseurl,downloaddirectory):
     url=url.replace(baseurl,'')
     path=downloaddirectory+url
     directory=os.path.dirname(path)
-
     if not os.path.exists(directory):
         os.makedirs(directory)
     return path
 
 html=urlopen(baseurl)
 bs4=BeautifulSoup(html)
-downloadedlist=bs4.findAll('img')#注意之前这里的用法，src=True or src=false
+downloadedlist=bs4.findAll(src=True)#注意之前这里的用法，src=True or src=false
 for downloaded in downloadedlist:
     # print(downloaded+'-----mark')
     url=getabsoluteurl(downloaded['src'])
     if url is not None:
         print(url)
-        urlretrieve(url,downloadpath(url,baseurl,downloaddirectory))
+        try:
+            # if "jpg" not in  url.split('/')[-1]:#只要还用urlretrieve，就无法保存一些后缀不是文件名的链接对应的文件
+            urlretrieve(url,downloadpath(url,baseurl,downloaddirectory))
+        except OSError as e :
+            # raise e #有这个好像会报错，没有就跳过继续执行
+            print("osError")
 
 
 
