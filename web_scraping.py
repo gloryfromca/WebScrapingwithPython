ded274d437dc662799a515ede522b2d43f67df90
zhanghui <huizhang1995@gmail.com>
2017-02-26 15:10:10 +0800

    twitter API usage of three common requests

--- web_scraping.py
+++ web_scraping.py
@@ -1,4 +1,9 @@
+
 from twitter import Twitter,OAuth
 t=Twitter (auth=OAuth("2875649376-YWYc87J6iw7zd3KLqinfeyjtr0XpwDMOANR3VTh","sFepdYSVgg3MUhUV0W2WzKIGiTbbvDV7aSxslSvRbuHkR","xZV0qZNKdgrWQnZTw6KWbKEYm","9fQsWNwDBF3YROF8NljKPdHJnrt7gQUCxR0uy92q6efhRZjAQ0"))
-pythontweets=t.search.tweets(q='#python')
-print(pythontweets)
+# pythontweets=t.search.tweets(q='#python')
+# print(pythontweets)
+# statusupdate=t.statuses.update(status='hello,world')
+# print(statusupdate)
+pythonstatuses=t.statuses.user_timeline(screen_name="montypython",count=5)
+print(pythonstatuses)
