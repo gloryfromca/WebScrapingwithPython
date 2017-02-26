b3ef372c0bd7033ea7d770e262459e02b3055106
zhanghui <huizhang1995@gmail.com>
2017-02-26 23:54:57 +0800

    download img from a website by os.path&urlretrieve

--- web_scraping.py
+++ web_scraping.py
@@ -1,63 +1,46 @@
-
-#https://en.wikipedia.org/w/index.php?title=Jadunath_Singh&offset=&action=history
-#https://en.wikipedia.org/wiki/Jadunath_Singh
-import datetime 
+import os
+from urllib.request import urlopen  
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
-import random
-import re
-import json
+from urllib.request import urlretrieve
 
-random.seed(datetime.datetime.now())
+baseurl="http://pythonscraping.com"
+downloaddirectory='downloaded'
 
-def getCountry(ipAddress):
-    try:
-        response = urlopen("http://freegeoip.net/json/"
-        +ipAddress).read().decode('utf-8')
-    except HTTPError:
+
+def getabsoluteurl(url):
+    if url.startswith('http://www.'): 
+        url=url.replace('http://www.','http://')#必须注意url=，没有这个，url本身不变化。
+    elif url.startswith("www."):
+        url=url.replace('www.','http://')
+    elif url.startswith("http://"):
+        url=url
+    elif url.startswith("/"):
+        url=baseurl+url
+    else:
+        url=baseurl+"/"+url
+
+    if baseurl not in url:
         return None
-    responseJson = json.loads(response)
-    return responseJson.get("country_code")
+    return url
+def downloadpath(url,baseurl,downloaddirectory):
+    url=url.replace(baseurl,'')
+    path=downloaddirectory+url
+    directory=os.path.dirname(path)
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    return path
+
+html=urlopen(baseurl)
+bs4=BeautifulSoup(html)
+downloadedlist=bs4.findAll('img')#注意之前这里的用法，src=True or src=false
+for downloaded in downloadedlist:
+    # print(downloaded+'-----mark')
+    url=getabsoluteurl(downloaded['src'])
+    if url is not None:
+        print(url)
+        urlretrieve(url,downloadpath(url,baseurl,downloaddirectory))
 
 
-def getlinks(link):#whole url  input and  whole url  output
-    html=urlopen(link)
-    bs4=BeautifulSoup(html)
-    links=set()
-    if bs4.find("div",{"id":"mw-content-text"})  is not None:
-        for link in bs4.find("div",{"id":"mw-content-text"}).findAll("a",href=re.compile("^(/wiki/)((?!:).)*$")):
-            link="https://en.wikipedia.org"+link.attrs['href']
-            if link not in links:
-                links.add(link) 
-        return links
-    else:
-        return []
-
-def gethistoryips(link):
-    history_url=link.replace("/wiki/","/w/index.php?title=")+"&limit=250&action=history"
-    html=urlopen(history_url)
-    bs4=BeautifulSoup(html)
-    ips=set()
-    if  bs4.find("ul",{"id":"pagehistory"}) is not None:
-        for ip in bs4.find("ul",{"id":"pagehistory"}).findAll("a",{"class":"mw-userlink mw-anonuserlink"}):#findAll可能是一个list或者set
-            ip=ip.get_text()
-            if ip  not in ips:
-                ips.add(ip)
-        return ips
-    else:
-        return []
 
 
-deep=1
-i=0
-links=getlinks('https://en.wikipedia.org/wiki/Python_(programming_language)')
-while(len(links)>0 and i<deep):
-    for link in links:
-        print('----------------')
-        historyips=gethistoryips(link)
-        for historyip in historyips:
-            if ":" not in historyip:
-                print(historyip +"  is from  "+ getCountry(historyip))
-    newlink=links[random.randint(0,len(links)-1)]
-    links=getlinks(newlink)
-    i=i+1
