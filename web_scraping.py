a3cd2b3226c4d59fd3318e23ec7555986d265c32
zhanghui_company <huizhang1995@gmail.com>
2017-02-27 13:01:53 +0800

    scrapy the table,the put it to CSV file

--- web_scraping.py
+++ web_scraping.py
@@ -1,9 +1,24 @@
 import csv
-csvfile=open('csv/test.csv','w+')#open方法
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html=urlopen("https://en.wikipedia.org/wiki/Comparison_of_text_editors")
+bs4=BeautifulSoup(html)
+trs=bs4.find('table',{"class":"wikitable"}).findAll('tr')# a little hard code
+#class="wikitable sortable jquery-tablesorter" class just use "wikitable"attribute 
+csvFile = open("csv/editors.csv",'wt',encoding='UTF-8')
+#Python 3 opens text files in the locale default encoding
+writer = csv.writer(csvFile)
 try:
-    writer=csv.writer(csvfile)
-    writer.writerow(("first column","second column","third column"))
-    for i in range(10):
-        writer.writerow((i,i+2,i*2))
+    for tr in trs:
+        row=[]
+        for th in tr.findAll(['td','th']):
+            th=th.get_text()
+            row.append(th)#"add" is usage of set
+            print(th)
+        writer.writerow(row)
 finally:
-    csvfile.close()#open打开的文件关掉
+    csvFile.close()
+
+
+
