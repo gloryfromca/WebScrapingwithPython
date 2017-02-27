e4f0971e83780329a49a2917550af09c6b578074
zhanghui_company <huizhang1995@gmail.com>
2017-02-27 17:16:31 +0800

    store title and content by using mysql from links beginning with a starturl

--- web_scraping.py
+++ web_scraping.py
@@ -1,24 +1,52 @@
-import csv
-from urllib.request import urlopen
+import pymysql
+from urllib.request import urlopen 
 from bs4 import BeautifulSoup
+import re
+import random
+import datetime
 
-html=urlopen("https://en.wikipedia.org/wiki/Comparison_of_text_editors")
-bs4=BeautifulSoup(html)
-trs=bs4.find('table',{"class":"wikitable"}).findAll('tr')# a little hard code
-#class="wikitable sortable jquery-tablesorter" class just use "wikitable"attribute 
-csvFile = open("csv/editors.csv",'wt',encoding='UTF-8')
-#Python 3 opens text files in the locale default encoding
-writer = csv.writer(csvFile)
+
+
+#连接数据库，使用数据库scraping，生成游标
+conn = pymysql.connect(host='127.0.0.1', 
+user='root', passwd='password', db='mysql', charset='utf8')
+cur = conn.cursor()
+cur.execute("USE scraping")
+
+random.seed(datetime.datetime.now())
+start_url="http://en.wikipedia.org"
+
+
+def get_links(url):# whole url input and whole url output 
+    html=urlopen(url)
+    bs4=BeautifulSoup(html)
+    links=[]
+    for link in  bs4.find("div",{"class":"mw-body-content"}).findAll("a",href=re.compile('^(/wiki/)((?!:).)*')):
+        link=link.attrs['href']
+        links.append(start_url+link)
+    return links
+
+def store(title,content):
+    cur.execute('insert into pages(title,content) values(\"%s\",\"%s\")',(title,content))
+    cur.connection.commit()
+
+def store_title_content(url):
+    html=urlopen(url)
+    bs4=BeautifulSoup(html)
+    title=bs4.find("h1",{"class":"firstHeading"}).get_text()
+    #<h1 id="firstHeading" class="firstHeading" lang="en">Kevin Bacon</h1>
+    content=bs4.find("div", {"id":"mw-content-text"}).find("p").get_text()
+    store(title,content)
+
+
+links=get_links(start_url)
 try:
-    for tr in trs:
-        row=[]
-        for th in tr.findAll(['td','th']):
-            th=th.get_text()
-            row.append(th)#"add" is usage of set
-            print(th)
-        writer.writerow(row)
+    while len(links)>0:
+        link=links[random.randint(0,len(links)-1)]
+        store_title_content(link)
+        print('storing is OK!')
+        links=get_links(link)
 finally:
-    csvFile.close()
-
-
+    cur.close()
+    conn.close()
 
