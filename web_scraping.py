8cf8fac806608d28043f99dec7b3a0cf7e11a2c5
zhanghui <huizhang1995@gmail.com>
2017-03-11 18:15:30 +0800

    get Bacon number by using mysql database v1.0

--- web_scraping.py
+++ web_scraping.py
@@ -11,7 +11,11 @@ import datetime
 conn = pymysql.connect(host='127.0.0.1', 
 user='root', passwd='password', db='mysql', charset='utf8')
 cur = conn.cursor()
+
+cur.execute("create database if not exists wikipedia")
 cur.execute("USE wikipedia")
+cur.execute("create table if not exists wikipedia.pages (id int not null AUTO_INCREMENT,url varchar(255) not null,created timestamp not null default current_timestamp, primary key(id)) ")
+cur.execute("create table if not exists wikipedia.links (id int not null AUTO_INCREMENT, frompageid int null,topageid int null,created timestamp not null default current_timestamp,primary key(id ))")
 
 random.seed(datetime.datetime.now())
 start_url="http://en.wikipedia.org"
@@ -27,25 +31,29 @@ def insert_page_if_not_exists(url):
         return cur.fetchone()[0]
 
 
-def insert_to_links():
-    cur.execute()
+def insert_to_links(frompageid,topageid):
+    cur.execute('select * from links where frompageid=%s and topageid=%s',(int(frompageid),int(topageid)))
+    if cur.rowcount==0:
+        cur.execute("INSERT INTO links (frompageid,topageid) VALUES (%s,%s)",(int(frompageid),int(topageid))) 
+        cur.connection.commit()
     
-links=set()#可以改写为从数据库中查询
+pages=set()#可以改写为从数据库中查询
 def get_links(url,recursionlevel):# whole url input and whole url output 
-
-    global links
-    pageid=insert_page_if_not_exists(url)
-
-    if  recursionlevel>5:
+    global pages
+    if  recursionlevel>2:#not 5 cause saving time
         return ;
-    html=urlopen(url)
+
+    pageid=insert_page_if_not_exists(url)
+    html=urlopen(start_url+url)
     bs4=BeautifulSoup(html)
 
-    for link in  bs4.find("div",{"class":"mw-body-content"}).findAll("a",href=re.compile('^(/wiki/)((?!:).)*')):
-        link=start_url+link.attrs['href']
+    for link in  bs4.findAll("a",href=re.compile('^(/wiki/)((?!:).)*')):
+        link=link.attrs['href']
         insert_to_links(pageid,insert_page_if_not_exists(link))
-        links.add(link)
-        if link not in links:
+        pages.add(link)
+        if link not in pages:
             get_links(link,recursionlevel+1)
 
-get_links(start_url,0)
+get_links("/wiki/Kevin_Bacon",0)
+cur.close()
+conn.close()
