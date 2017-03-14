d1d4636f82cb7e326a18036b906c2583ac1d5c80
zhanghui <huizhang1995@gmail.com>
2017-03-14 20:57:47 +0800

    reading documents -text v0.2

--- web_scraping.py
+++ web_scraping.py
@@ -5,9 +5,10 @@ import re
 import random
 import datetime
 
-textpage=urlopen("http://www.pythonscraping.com/pages/warandpeace/chapter1-ru.txt")
-# print(type(textpage.read()))
-# print(textpage.read())#好像只能read（）方法只能读取一次，所以先调用后赋值
-s=textpage.read().decode('utf-8')
-# s=str(textpage.read(),'utf-8')#equal to one above  
 
+html = urlopen("http://en.wikipedia.org/wiki/Python_(programming_language)")
+bs=BeautifulSoup(html)
+text=bs.find('div',{'id':'mw-content-text'}).get_text()
+content=bytes(text,'utf-8')
+content=content.decode('utf-8')
+print(content)
