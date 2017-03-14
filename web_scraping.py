29699aa26f66834d043a2da309473711c7389ed8
zhanghui <huizhang1995@gmail.com>
2017-03-14 22:50:08 +0800

    reading documents -csv v0.2

--- web_scraping.py
+++ web_scraping.py
@@ -5,19 +5,15 @@ import re
 import random
 import datetime
 import csv
-from io import StringIO
+from io import StringIO ,open
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFResourceManager , process_pdf
+from pdfminer.layout import LAParams
 
 
 data = urlopen("http://pythonscraping.com/files/MontyPythonAlbums.csv").read().decode('ascii', 'ignore')
-print(type(data))
-print(data)#也是可以的，类型是字符串str
-print('-----------------')
 datafile=StringIO(data)
-print(type(datafile))
-print(datafile)#打印出来的是object，一个对象
-print('-----------------')
-csvReader=csv.reader(datafile)
+csvReader=csv.DictReader(datafile)
 for row in csvReader:
-    # print(row)#打印出来的是一行行list对象
-    print('the album \"'+row[0]+"\" was released in"+row[1])
+    print(row)
 
