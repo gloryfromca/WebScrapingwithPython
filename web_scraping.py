978ef6c2941e1ed2a7ee74e81ebc785c530617dc
zhanghui <huizhang1995@gmail.com>
2017-03-15 16:20:33 +0800

    cleaning dirty data n-grams v0.1

--- web_scraping.py
+++ web_scraping.py
@@ -1,40 +1,20 @@
 import pymysql
 from urllib.request import urlopen 
 from bs4 import BeautifulSoup
-import re
-import random
-import datetime
-import csv
-from io import StringIO ,BytesIO,open
-from pdfminer.converter import TextConverter
-from pdfminer.pdfinterp import PDFResourceManager , process_pdf
-from pdfminer.layout import LAParams
-from zipfile import ZipFile
 
-wordfile=urlopen("http://pythonscraping.com/pages/AWordDocument.docx").read()#type:bytes
-wordfile=BytesIO(wordfile)#type:io.BytesIO object
-document=ZipFile(wordfile)#type:zipfile.ZipFile file=<_io.BytesIO object at 0x0000000003582990> mode='r'
-xml_content=document.read('word/document.xml')#type:bytes
+def ngrams(input,n):
+    input=input.split(' ')
+    output=[]
+    for i in range(len(input)+n-1):
+        output.append(input[i:i+n])
+    return output
 
-s=xml_content.decode('utf-8')#type:str
-
-# print(s)
-
-bs4=BeautifulSoup(s)
-textelems=bs4.findAll('w:t')
-for textelem in textelems:
-    closetag=''
-    try:
-        style=textelem.parent.previousSibling.find("w:pstyle")
-        if style is not None and style['w:val']=='Title':
-            print('<h1>')
-            closetag="</h1>"
-    except  AttributeError:
-        pass
-    print(textelem.text)
-    print(closetag)
-
-
-            
+html=urlopen("http://en.wikipedia.org/wiki/Python_(programming_language)")#type:class http.client.HTTPResponse
+bs4=BeautifulSoup(html)
+content = bs4.find("div", {"id":"mw-content-text"}).get_text()
+n=2
+ngrams = ngrams(content, n)   
+print(ngrams)
+print(str(n)+'-grams count is '+str(len(ngrams)))
 
 
