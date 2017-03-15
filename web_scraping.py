0c19ae43fb472df03b1f48cb4853b546729a5128
zhanghui <huizhang1995@gmail.com>
2017-03-15 16:29:35 +0800

    cleaning dirty data n-grams v0.2

--- web_scraping.py
+++ web_scraping.py
@@ -1,12 +1,18 @@
 import pymysql
 from urllib.request import urlopen 
 from bs4 import BeautifulSoup
+import re
 
-def ngrams(input,n):
-    input=input.split(' ')
+def ngrams(content,n):
+    content=re.sub('\n+'," ",content)
+    content=re.sub(' +'," ",content)
+    content=bytes(content,'utf-8')
+    content=content.decode('ascii','ignore')
+
+    content=content.split(' ')
     output=[]
-    for i in range(len(input)+n-1):
-        output.append(input[i:i+n])
+    for i in range(len(content)+n-1):
+        output.append(content[i:i+n])
     return output
 
 html=urlopen("http://en.wikipedia.org/wiki/Python_(programming_language)")#type:class http.client.HTTPResponse
