1e64a0fb10fee4b8dfc427fa59a89139bd61b9c0
zhanghui <huizhang1995@gmail.com>
2017-03-18 00:56:06 +0800

    example for usages of nltk :tag  words in text with word-type(e.g.: Vi. Vt. N.) v0.2

--- web_scraping.py
+++ web_scraping.py
@@ -1,53 +1,24 @@
-import pymysql
-from urllib.request import urlopen 
-from bs4 import BeautifulSoup
-import re
-import string
-from collections import OrderedDict,Counter
-from  random import randint
+import nltk
+from  nltk import sent_tokenize,word_tokenize,Text,pos_tag
+# from nltk.book import  *
 
-conn=pymysql.connect(host='127.0.0.1',user='root',passwd='password',charset='utf8')
-cur=conn.cursor()
-cur.execute('use wikipedia')
+sample1="Strange women lying in ponds distributing swords is no\
+basis for a system of government. Supreme executive power derives from a mandate\
+from the masses, not from some farcical aquatic ceremony."
+sample2="The dust was thick so he had to dust"
+sample3="Google is one of the best companies in the world.\
+I constantly google myself to see what I'm up to."
+nouns=['NN', 'NNS', 'NNP', 'NNPS']
 
-class SolutionFound(RuntimeError):
-    def __init__(self, message):
-        self.message = message
-        
-def getlinks(currentid):
-    cur.execute('select topageid from links where frompageid=%s',(currentid))
-    if cur.rowcount==0:
-        return None
-    else:
-        return [x[0] for x in cur.fetchall()]
-def constructDict(currentid):
-    links_list=getlinks(currentid)
-    if links_list is None:
-        return {}
-    a=dict(zip(links_list,[{}]*len(links_list)))
-    return a
+sentences=sent_tokenize(sample3)
+for sentence in sentences:
+    if "google" in sentence.lower():
+        taggedwords=pos_tag(word_tokenize(sentence))
+        for  word in taggedwords:
+            if word[0].lower()=="google" and word[1] in nouns:
+                print(sentence)
 
 
-def searchDepth(targetPageid,currentid,linkTree,depth):
-    if depth==0:
-        return linkTree
-    if not linkTree:
-        linkTree=constructDict(currentid)
-        if not linkTree:
-            return {}
-    if targetPageid in linkTree.keys():
-        print('target_id: '+str(targetPageid)+' FOUND!')
-        raise SolutionFound("process_id:"+str(currentid))
-    for key,value in linkTree.items():
-        try:
-            linkTree[key]=searchDepth(targetPageid,key,linkTree[key],depth-1)
-        except SolutionFound as e:
-            print(e.message)
-            raise SolutionFound("page:"+str(currentid))
-    return linkTree
-try:
-    s=searchDepth(156,2,{},4)
-    print('NO solution found')
-except SolutionFound as e:
-    print(' start_id: '+e.message.split(':')[1])
-#错误的层层抛出来<-->找到结果后search过程经过的id层层抛出
+
+
+
