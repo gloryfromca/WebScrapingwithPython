5cd0acbabfcba922344de9a26fe0a8976578dea0
zhanghui <huizhang1995@gmail.com>
2017-03-18 02:34:03 +0800

    crawling through forms and logins v0.4: usage of session

--- web_scraping.py
+++ web_scraping.py
@@ -1,9 +1,11 @@
 import requests
+session=requests.Session()
+
 params={'username': 'zhanghui', 'password': 'password'}
-r=requests.post("http://pythonscraping.com/pages/cookies/welcome.php", data=params)
+s=session.post("http://pythonscraping.com/pages/cookies/welcome.php", data=params)
 print('Cookie is set to:')
-print(r.cookies.get_dict())
+print(s.cookies.get_dict())
 print('-------------')
 print('Now go to the profile page...')
-new_r=requests.get("http://pythonscraping.com/pages/cookies/profile.php",cookies=r.cookies)
-print(new_r.text)
+new_s=session.get("http://pythonscraping.com/pages/cookies/profile.php",cookies=s.cookies)
+print(new_s.text)
