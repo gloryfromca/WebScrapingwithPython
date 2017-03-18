48bb676f97b35590f117ccfaed09114b669a97bf
zhanghui <huizhang1995@gmail.com>
2017-03-18 17:32:00 +0800

    executing JavaScript in Python with Selenium v0.1: the usages of WebDriverWait and expected_conditions

--- web_scraping.py
+++ web_scraping.py
@@ -1,6 +1,14 @@
-import requests
-from requests.auth import AuthBase 
-from requests.auth import HTTPBasicAuth 
-auth=HTTPBasicAuth('zhanghui','password')
-r=requests.post(url="http://pythonscraping.com/pages/auth/login.php",auth=auth)
-print(r.text)
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver=webdriver.PhantomJS(executable_path='C:/Users/Administrator/Downloads/phantomjs-2.1.1-windows/bin/phantomjs.exe')
+driver.get("http://pythonscraping.com/pages/javascript/ajaxDemo.html")
+try:
+    element=WebDriverWait(driver,10).until(EC.presence_of_element_located((By.ID,'loadedButton')))
+finally:
+    print(driver.find_element_by_id('content').text)
+    driver.close()
+
+
