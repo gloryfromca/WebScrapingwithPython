4a047b81e451d13e965129a7446125958531df2b
zhanghui <huizhang1995@gmail.com>
2017-03-19 21:50:45 +0800

    handle client-side redirects

--- web_scraping.py
+++ web_scraping.py
@@ -1,18 +1,29 @@
 import pdb
+# pdb.set_trace()
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException
 
+def webforload(driver):
+    count=0
+    while True:
+        element = driver.find_element_by_tag_name("html")
+        count+=1
+        if count>100:
+            print("Timeout happened after 10 seconds")
+            return
+        time.sleep(0.5)
+        if element != driver.find_element_by_tag_name('html'):
+            return
+
+
+        
 driver=webdriver.PhantomJS(executable_path='C:/Users/Administrator/Downloads/phantomjs-2.1.1-windows/bin/phantomjs.exe')
-# print(type(driver))#type:class 'selenium.webdriver.phantomjs.webdriver.WebDriver'
-# pdb.set_trace()
-driver.get("http://pythonscraping.com/pages/javascript/ajaxDemo.html")
-try:
-    element=WebDriverWait(driver,10).until(EC.presence_of_element_located((By.ID,'loadedButton')))#使driver等待EC后抓取，返回的是element located（这里是：A button to click!）
-    # 也可以不用返回element
-finally:
-    print(driver.find_element_by_id('content').text)
-    driver.close()
+driver.get("http://pythonscraping.com/pages/javascript/redirectDemo1.html")
+webforload(driver)
+print(driver.page_source)#print web_page from PhantomJS drove by 'driver'
 
 
