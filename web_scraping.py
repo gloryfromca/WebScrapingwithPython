d64f3e7aa2a4cae0a8d81da374097f112d8ba06b
zhanghui <huizhang1995@gmail.com>
2017-03-21 00:20:38 +0800

    Scraping Text from Images on Websites

--- web_scraping.py
+++ web_scraping.py
@@ -1,24 +1,29 @@
 import pdb
 # pdb.set_trace()
-from PIL import Image,ImageFilter 
+from selenium import webdriver 
+import time
+from urllib.request import urlretrieve
 import subprocess
 
-def clean_gray(path):
-    img=Image.open(path)
-    img=img.point(lambda x:0 if x<143 else 255)
-    new_path=path.split(".")[0]+'_clean.'+path.split(".")[1]
-    img.save(new_path)
-    return new_path
-
-def tesseract_print(path):
-    subprocess.call(['tesseract',path,"output"])
-    output=open("output.txt")
-    try:
-        print(output.read())
-    finally:
-        output.close()
-
-path="2.png"
-new_path=clean_gray(path)
-tesseract_print(new_path)
+driver=webdriver.PhantomJS(executable_path="C:/Users/Administrator/Downloads/phantomjs-2.1.1-windows/bin/phantomjs.exe")
+driver.get("http://www.amazon.com/War-Peace-Leo-Nikolayevich-Tolstoy/dp/1427030200")
+time.sleep(5)
+driver.find_element_by_id('sitbLogoImg').click()
+imageList=set()
+time.sleep(5)
+while 'pointer' in driver.find_element_by_id("sitbReaderRightPageTurner").get_attribute("style"):
+    driver.find_element_by_id("sitbReaderRightPageTurner").click()
+    time.sleep(2)
+    pages=driver.find_elements_by_xpath("//div[@class='pageImage']/div/img")
+    driver.find_element_by_id("sitbReaderRightPageTurner").click()
+    for page in pages:
+        image=page.get_attribute('src')
+        imageList.add(image)
+driver.quit()
+for image in sorted(imageList):
+    urlretirieve(image,'page.jpg')
+    p=subprocess.Popen(["tesseract", "page.jpg", "page"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    p.wait()    
+    f.open("page.txt","r")
+    print(f.read())
 
