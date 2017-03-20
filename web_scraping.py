5f438d7187b44e37edb1f113b16680f1d65ace72
zhanghui <huizhang1995@gmail.com>
2017-03-20 20:23:02 +0800

    Image Processing and Text Recognition v0.1: PIL.Image and PIL.ImageFilter

--- web_scraping.py
+++ web_scraping.py
@@ -1,29 +1,9 @@
 import pdb
 # pdb.set_trace()
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import StaleElementReferenceException
-
-def webforload(driver):
-    count=0
-    while True:
-        element = driver.find_element_by_tag_name("html")
-        count+=1
-        if count>100:
-            print("Timeout happened after 10 seconds")
-            return
-        time.sleep(0.5)
-        if element != driver.find_element_by_tag_name('html'):
-            return
-
-
-        
-driver=webdriver.PhantomJS(executable_path='C:/Users/Administrator/Downloads/phantomjs-2.1.1-windows/bin/phantomjs.exe')
-driver.get("http://pythonscraping.com/pages/javascript/redirectDemo1.html")
-webforload(driver)
-print(driver.page_source)#print web_page from PhantomJS drove by 'driver'
+from PIL import Image,ImageFilter 
+cat=Image.open("cat.jpg")
+blurrycat=cat.filter(ImageFilter.GaussianBlur)
+blurrycat.save("cat_blur.jpg")
+blurrycat.show()
 
 
