d610772c89b8b04dc4520a03fbc146d7aef5f368
zhanghui <huizhang1995@gmail.com>
2017-03-20 21:25:39 +0800

    Image Processing and Text Recognition v0.2: clean images and process them by tesseract

--- web_scraping.py
+++ web_scraping.py
@@ -1,9 +1,24 @@
 import pdb
 # pdb.set_trace()
 from PIL import Image,ImageFilter 
-cat=Image.open("cat.jpg")
-blurrycat=cat.filter(ImageFilter.GaussianBlur)
-blurrycat.save("cat_blur.jpg")
-blurrycat.show()
+import subprocess
 
+def clean_gray(path):
+    img=Image.open(path)
+    img=img.point(lambda x:0 if x<143 else 255)
+    new_path=path.split(".")[0]+'_clean.'+path.split(".")[1]
+    img.save(new_path)
+    return new_path
+
+def tesseract_print(path):
+    subprocess.call(['tesseract',path,"output"])
+    output=open("output.txt")
+    try:
+        print(output.read())
+    finally:
+        output.close()
+
+path="2.png"
+new_path=clean_gray(path)
+tesseract_print(new_path)
 
