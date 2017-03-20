2b7272b6c956c321274919527ee22ac6dbfc6746
zhanghui <huizhang1995@gmail.com>
2017-03-21 02:44:19 +0800

    Looking Like a Human: Handling Cookies

--- web_scraping.py
+++ web_scraping.py
@@ -1,13 +1,5 @@
-from bs4 import BeautifulSoup
-import requests
-
-session=requests.Session()
-headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5)\
-AppleWebKit 537.36 (KHTML, like Gecko) Chrome",
-"Accept":"text/html,application/xhtml+xml,application/xml;\
-q=0.9,image/webp,*/*;q=0.8"}
-url = "https://www.whatismybrowser.com/\
-developers/what-http-headers-is-my-browser-sending"
-req = session.get(url, headers=headers)
-bsObj = BeautifulSoup(req.text)
-print(bsObj.find("table",{"class":"table-striped"}).get_text)
+from selenium import webdriver
+driver = webdriver.PhantomJS(executable_path='C:/Users/Administrator/Downloads/phantomjs-2.1.1-windows/bin/phantomjs.exe')
+driver.get("http://pythonscraping.com")
+driver.implicitly_wait(1)
+print(driver.get_cookies())
