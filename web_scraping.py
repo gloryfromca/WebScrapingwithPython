fdfa40bbc3a65e37e801124d7a6c60b0a7558f09
zhanghui <huizhang1995@gmail.com>
2017-03-21 23:24:12 +0800

    Testing with Selenium : Taking screenshots

--- web_scraping.py
+++ web_scraping.py
@@ -1,12 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver import ActionChains
-
-driver = webdriver.PhantomJS(executable_path='C:/Users/Administrator/Downloads/phantomjs-2.1.1-windows/bin/phantomjs.exe')
-driver.get('http://pythonscraping.com/pages/javascript/draggableDemo.html')
-print(driver.find_element_by_id("message").text)
-element = driver.find_element_by_id("draggable")
-target = driver.find_element_by_id("div2")
-actions = ActionChains(driver)
-actions.drag_and_drop(element, target).perform()
-print(driver.find_element_by_id("message").text)
+driver = webdriver.PhantomJS('C:/Users/Administrator/Downloads/phantomjs-2.1.1-windows/bin/phantomjs.exe')
+driver.get('http://www.pythonscraping.com/')
+driver.get_screenshot_as_file('pythonscraping.png')
