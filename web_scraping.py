9f98d531015829143d826653c7b243acf8031e99
zhanghui <huizhang1995@gmail.com>
2017-03-21 22:47:35 +0800

    Testing with Selenium : Drag and drop

--- web_scraping.py
+++ web_scraping.py
@@ -1,47 +1,12 @@
-from bs4 import BeautifulSoup
-import unittest
-from urllib.request import urlopen
-import re
+from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver import ActionChains
 
-class TestWikipedia(unittest.TestCase):
-    url=None
-    bs4=None
-
-    def test_Pageproperties(self):
-        global url
-        global bs4
-        url="http://en.wikipedia.org/wiki/Monty_Python"
-        for i in range(1,10):
-            titles=self.titleMatchurl()
-            self.assertEquals(titles[0],titles[1])
-            self.assertTrue(self.contentExists())
-            url=self.nextUrl()
-            print("Done!")
-    def titleMatchurl(self):
-        global url 
-        global bs4
-        bs4=BeautifulSoup(urlopen(url))
-        title=bs4.find("h1").get_text()
-        urltitle=url[(url.index('/wiki/')+6):]
-        urltitle=urltitle.replace("_"," ")
-        return [title.lower(),urltitle.lower()]
-    def contentExists(self):
-        global bs4
-
-        content = bs4.find("div",{"id":"mw-content-text"})
-        if content is not None:
-            return True
-        return False
-    def nextUrl(self):
-        global bs4
-        url="http://en.wikipedia.org"+bs4.find("div", {"id":"bodyContent"}).find("a",href=re.compile("^(/wiki/)((?!:).)*$"))['href']
-        print(url)
-        return url
-if __name__ == '__main__':
-    unittest.main()
-
-
-
-        
-
-
+driver = webdriver.PhantomJS(executable_path='C:/Users/Administrator/Downloads/phantomjs-2.1.1-windows/bin/phantomjs.exe')
+driver.get('http://pythonscraping.com/pages/javascript/draggableDemo.html')
+print(driver.find_element_by_id("message").text)
+element = driver.find_element_by_id("draggable")
+target = driver.find_element_by_id("div2")
+actions = ActionChains(driver)
+actions.drag_and_drop(element, target).perform()
+print(driver.find_element_by_id("message").text)
